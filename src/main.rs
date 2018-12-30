c97554de2fb3b6fd4efc7639695f8e0f169b14e9
cat_in_136 <cat.in.136+github@gmail.com>
2018-12-31 01:44:56 +0900

    add increment/decrement pointer

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ pub enum BFIError {
     Io(std::io::Error),
     MissingClosingBrackets,
     MissingOpeningBrackets,
+    MemoryOverRun,
 }
 
 impl std::fmt::Display for BFIError {
@@ -15,6 +16,7 @@ impl std::fmt::Display for BFIError {
             BFIError::Io(ref err) => write!(f, "{}", err),
             BFIError::MissingClosingBrackets => write!(f, "Missing closing bracket(s)"),
             BFIError::MissingOpeningBrackets => write!(f, "Missing opening bracket(s)"),
+            BFIError::MemoryOverRun => write!(f, "Pointer moved to out of range of memory"),
         }
     }
 }
@@ -25,14 +27,19 @@ impl From<std::io::Error> for BFIError {
     }
 }
 
+#[derive(Debug)]
 pub struct BFI {
+    x: Vec<i32>,
     c: String,
+    pc: usize,
 }
 
 impl BFI {
     pub fn new(s: String) -> Self {
         Self {
+            x: vec![0; 32767 + 1],
             c: s,
+            pc: 0,
         }
     }
 
@@ -63,13 +70,55 @@ impl BFI {
             Ok(())
         }
     }
+
+    fn increment_pointer(&mut self) -> Result<(), BFIError> {
+        if self.pc + 1 >= self.x.len() {
+            Err(BFIError::MemoryOverRun)
+        } else {
+            self.pc += 1;
+            Ok(())
+        }
+    }
+
+    fn decrement_pointer(&mut self) -> Result<(), BFIError> {
+        if self.pc <= 0 {
+            Err(BFIError::MemoryOverRun)
+        } else {
+            self.pc -= 1;
+            Ok(())
+        }
+    }
+
+    pub fn interpret(&mut self) -> Result<(), BFIError> {
+        let chars_length = self.c.len();
+
+        self.pc = 0;
+        while self.pc < chars_length {
+            let c = self.c.get(self.pc..=self.pc);
+
+            match c {
+                Some(">") => self.increment_pointer()?,
+                Some("<") => self.decrement_pointer()?,
+                Some("+") => (),
+                Some("-") => (),
+                Some(".") => (),
+                Some(",") => (),
+                Some("[") => (),
+                Some("]") => (),
+                _ => (),
+            };
+
+            self.pc += 1;
+        }
+        Ok(())
+    }
 }
 
 fn main() -> Result<(), BFIError> {
     for argument in env::args().skip(1) {
-        let bfi = BFI::from_file(argument)?;
+        let mut bfi = BFI::from_file(argument)?;
         bfi.check_syntax()?;
-        println!("{}", bfi.c);
+        bfi.interpret()?;
     }
     Ok(())
 }
@@ -97,6 +146,46 @@ mod tests {
         } else {
             false
         });
+    }
+
+    #[test]
+    fn test_check_increment_pointer() {
+        let mut bfi = BFI::new(".".to_string());
+        assert_eq!(bfi.pc, 0);
+        bfi.increment_pointer().unwrap();
+        assert_eq!(bfi.pc, 1);
+        bfi.increment_pointer().unwrap();
+        assert_eq!(bfi.pc, 2);
+
+        bfi.pc = bfi.x.len() - 2;
+        bfi.increment_pointer().unwrap();
+        assert_eq!(bfi.pc, bfi.x.len() - 1);
+        assert!(if let BFIError::MemoryOverRun = bfi.increment_pointer().unwrap_err() {
+            true
+        } else {
+            false
+        });
+        assert_eq!(bfi.pc, bfi.x.len() - 1);
+    }
+
+    #[test]
+    fn test_check_decrement_pointer() {
+        let mut bfi = BFI::new(".".to_string());
+        assert_eq!(bfi.pc, 0);
+        assert!(if let BFIError::MemoryOverRun = bfi.decrement_pointer().unwrap_err() {
+            true
+        } else {
+            false
+        });
+        assert_eq!(bfi.pc, 0);
+
+        bfi.pc = 1;
+        bfi.decrement_pointer().unwrap();
+        assert_eq!(bfi.pc, 0);
+
+        bfi.pc = bfi.x.len() - 1;
+        bfi.decrement_pointer().unwrap();
+        assert_eq!(bfi.pc, bfi.x.len() - 2);
     }
 }
 
