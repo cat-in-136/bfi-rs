f7abf34fbf6048dfd3dc7afd02c537e0d257f953
cat_in_136 <cat.in.136+github@gmail.com>
2018-12-31 17:33:56 +0900

    add license code.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+/*
+ *  bfi-rs (Brainfuck Interpreter by Rust)
+ *  Copyright (C) 2018 cat-in-136
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ *
+ *  The Original Code is http://esoteric.sange.fi/brainfuck/impl/interp/BFI.java
+ *  The Initial Developer of the Original Code is Thomas Cort.
+ *  Portions created by the Initial Developer are Copyright (C) 2003
+ *  the Initial Developer under GPL.
+ */
+
 use std::env;
 use std::fs::File;
 use std::i8;
